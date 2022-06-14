1baee42ce3704a7f6f75e8303fed7779cbdf1a2f
C0A21016 <c0a2101667@edu.teu.ac.jp>
2022-06-14 16:12:07 +0900

    直し #1

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,18 +1,33 @@
 
 import random
 import datetime
-shutudai()
+
+
 def shutudai():
     Q1 = "サザエの旦那の名前は？"
     Q2 = "カツオの妹の名前は？"
     Q3 = "タラオはカツオから見てどんな関係？"
     Q = [Q1,Q2,Q3]
+    print("問題 ")
+    m = random.randint(Q)
+    print(m)
+    return m
+def kaito(m):
     A1 = ["マスオ","ますお"]
     A2 = ["わかめ","わかめ"]
     A3 = ["甥","おい","甥っ子","おいっこ"]
-    prnt(input(random.randint(Q)))
-def kaito(ans):
-    if ans in A1:
-        print("正解!!")
+    if m == Q1:
+        if m in A1:
+            print("正解!!")
+    if m == Q2:
+        if m in A2:
+            print("正解!!")
+    if m == Q3:
+        if m in A3:
+            print("正解!!")
     else:
         print("出直してこい")
+
+if __name__ == "__main__":
+    shutudai()
+    kaito(m)
