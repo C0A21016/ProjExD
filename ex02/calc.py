db1277b25dc20fa4c5746000000a53f3d1758c73
C0A21016 <c0a2101667@edu.teu.ac.jp>
2022-06-21 16:46:39 +0900

    4限

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,3 +1,4 @@
+
 import tkinter as tk
 import tkinter.messagebox as tkm
 def button_click(event):
@@ -5,25 +6,41 @@ def button_click(event):
     num=button["text"]
     if num=="=":
         eq=entry.get()
-        re=eval(eq)
         entry.delete(0,tk.END)
+        re=eval(eq)
+        return entry.insert(tk.END,re)
+    elif num=="←":
+        et=entry.get()
+        aa = len(et)
+        entry.delete(aa-1,tk.END)
+    elif num=="p":
+        entry.delete(0,tk.END)
+        re="ぱわー！！"
         entry.insert(tk.END,re)
+    elif num=="Zz":
+        tkm.showinfo("寝よか")
+    elif num=="C":
+        entry.delete(0,tk.END)
+
+        
 
     else:
         entry.insert(tk.END,num)
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("300x600")
+    root.geometry("500x700")
     root.title("超高機能電卓")
     entry=tk.Entry(root, justify="right",width=10,font=("Times New Roman",40))
     entry.grid(column=0,columnspan=5)
     r,c=1,0
-    for i, num in enumerate([i for i in range(9,-1,-1)]+["+"]+["="]):
+    pi=3.14
+
+    for i, num in enumerate(["C","-","/","*","Zz",9,4,"p",8,"pi",7,6,5,3,"←",2,1,0,"+","="]):
         button = tk.Button(root,text=f"{num}", width=4,height=2, font=("Times New Roman", 30))
         button.bind("<1>",button_click)
         button.grid(row=r,column=c)
         c+=1
-        if (i+1)%3==0:
+        if (i+1)%5==0:
             r+=1
             c = 0
        
